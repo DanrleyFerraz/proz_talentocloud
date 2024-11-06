9dc3e03d937121eaf0bd99556493c6f9a1fd4724
DanrleyFerraz <danrley.braga@jala.university>
2024-11-06 00:41:46 -0300

    Atividade proz

--- atividade_arrys3.py
+++ atividade_arrys3.py
@@ -1,14 +1,4 @@
 lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores'] 
 
-lista_produtos.pop(1)
-lista_produtos.append("Rimel")
-lista_produtos.pop(3)
-lista_produtos.append("Cremes Hidratantes")
-lista_produtos.pop(5)
-lista_produtos.append("Condicionadores")
-lista_produtos.append("Reparador de Pontas")
-
-
-
-
-print(lista_produtos)
+for produtos in lista_produtos:
+    print(f"Temos {produtos} à venda!")
