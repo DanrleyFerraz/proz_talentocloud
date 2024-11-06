3e13d22665ae6f7984450e25e49f1671b6cbd227
DanrleyFerraz <danrley.braga@jala.university>
2024-11-05 23:33:45 -0300

    Atividade proz

---
+++ atividade_arrays.py
@@ -0,0 +1,6 @@
+produtos_materiais_construcao = ["cimento", "areia", "tijolo", "telha", "ferro"]
+
+anos_nascimento = [1995, 2000, 1998, 1987, 1993]
+
+print("Produtos de materiais de construção:", produtos_materiais_construcao)
+print("Anos de nascimento de familiares e amigos:", anos_nascimento)
